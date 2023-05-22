b34e717b53b3f7ca369922f8d45c10fef7ed1507
Milos Zivlak <zivlakmilos@gmail.com>
2023-05-22 22:35:17 +0200

    Lexer for indentifiers and keywords

--- src/lexer/__tests__/lexer.ts
+++ src/lexer/__tests__/lexer.ts
@@ -21,3 +21,69 @@ test('test getNextToken()', () => {
     expect(lexer.getNextToken().type).toBe(token);
   }
 });
+
+
+test('test getNextToken()', () => {
+  const input = `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+`;
+
+  const lexer = new Tokenizer(input);
+
+  const tokens = [
+    { type: TokenType.Let, literal: 'let' },
+    { type: TokenType.Ident, literal: 'five' },
+    { type: TokenType.Assign, literal: '=' },
+    { type: TokenType.Int, literal: '5' },
+    { type: TokenType.Semicolon, literal: ';' },
+
+
+    { type: TokenType.Let, literal: 'let' },
+    { type: TokenType.Ident, literal: 'ten' },
+    { type: TokenType.Assign, literal: '=' },
+    { type: TokenType.Int, literal: '10' },
+    { type: TokenType.Semicolon, literal: ';' },
+
+    { type: TokenType.Let, literal: 'let' },
+    { type: TokenType.Ident, literal: 'add' },
+    { type: TokenType.Assign, literal: '=' },
+    { type: TokenType.Function, literal: 'fn' },
+    { type: TokenType.LParen, literal: '(' },
+    { type: TokenType.Ident, literal: 'x' },
+    { type: TokenType.Comma, literal: ',' },
+    { type: TokenType.Ident, literal: 'y' },
+    { type: TokenType.RParen, literal: ')' },
+    { type: TokenType.LBrace, literal: '{' },
+
+    { type: TokenType.Ident, literal: 'x' },
+    { type: TokenType.Plus, literal: '+' },
+    { type: TokenType.Ident, literal: 'y' },
+    { type: TokenType.Semicolon, literal: ';' },
+
+    { type: TokenType.RBrace, literal: '}' },
+    { type: TokenType.Semicolon, literal: ';' },
+
+    { type: TokenType.Let, literal: 'let' },
+    { type: TokenType.Ident, literal: 'result' },
+    { type: TokenType.Assign, literal: '=' },
+    { type: TokenType.Ident, literal: 'add' },
+    { type: TokenType.LParen, literal: '(' },
+    { type: TokenType.Ident, literal: 'five' },
+    { type: TokenType.Comma, literal: ',' },
+    { type: TokenType.Ident, literal: 'ten' },
+    { type: TokenType.RParen, literal: ')' },
+    { type: TokenType.Semicolon, literal: ';' },
+  ]
+
+  for (let token of tokens) {
+    const currentToken = lexer.getNextToken();
+    expect(currentToken.type).toBe(token.type);
+    expect(currentToken.literal).toBe(token.literal);
+  }
+});
---
+++ src/lexer/lexer.ts
@@ -0,0 +1,156 @@
+export const TokenType = {
+  Illegal: 'ILLEGAL',
+  Eof: 'EOF',
+
+  // ientifiers + literals
+  Ident: 'IDENT',
+  Int: 'INT',
+
+  // operators
+  Assign: '=',
+  Plus: '+',
+
+  // delimeters
+  Comma: ',',
+  Semicolon: ';',
+
+  LParen: 'LPAREN',
+  RParen: 'RPAREN',
+  LBrace: 'LBRACE',
+  RBrace: 'RBRACE',
+
+  // keywords
+  Function: 'FUNCTION',
+  Let: 'LET',
+} as const;
+
+type TokenItem = typeof TokenType[keyof typeof TokenType];
+
+export type Token = {
+  type: TokenItem,
+  literal: string,
+}
+
+export const createToken = (type: TokenItem, literal: string): Token => {
+  return { type, literal }
+}
+
+const Keywords = {
+  fn: TokenType.Function,
+  let: TokenType.Let,
+} as const;
+
+export class Tokenizer {
+  private position: number = 0;
+  private nextPosition: number = 0;
+  private ch: string;
+
+  constructor(private input: string) {
+    this.readChar();
+  }
+
+  public getNextToken(): Token {
+    let tok: Token;
+
+    this.skipWhitespace();
+
+    switch (this.ch) {
+      case '=':
+        tok = createToken(TokenType.Assign, this.ch);
+        break;
+      case '+':
+        tok = createToken(TokenType.Plus, this.ch);
+        break;
+      case '(':
+        tok = createToken(TokenType.LParen, this.ch);
+        break;
+      case ')':
+        tok = createToken(TokenType.RParen, this.ch);
+        break;
+      case '{':
+        tok = createToken(TokenType.LBrace, this.ch);
+        break;
+      case '}':
+        tok = createToken(TokenType.RBrace, this.ch);
+        break;
+      case ',':
+        tok = createToken(TokenType.Comma, this.ch);
+        break;
+      case ';':
+        tok = createToken(TokenType.Semicolon, this.ch);
+        break;
+      case '\0':
+        tok = createToken(TokenType.Eof, this.ch);
+        break;
+      default:
+        if (this.isLetter(this.ch)) {
+          const literal = this.readIdentifier();
+          const type = this.lookupIdent(literal);
+          tok = createToken(type, literal);
+          return tok;
+        } else if (this.isDigit(this.ch)) {
+          const literal = this.readNumber();
+          const type = TokenType.Int;
+          tok = createToken(type, literal);
+          return tok;
+        } else {
+          tok = createToken(TokenType.Illegal, '');
+        }
+        break;
+    }
+
+    this.readChar();
+    return tok;
+  }
+
+  private readChar(): void {
+    if (this.nextPosition < this.input.length) {
+      this.ch = this.input[this.nextPosition];
+    } else {
+      this.ch = '\0';
+    }
+
+    this.position = this.nextPosition;
+    this.nextPosition++;
+  }
+
+  private readIdentifier(): string {
+    const position = this.position;
+    while (this.isLetter(this.ch)) {
+      this.readChar();
+    }
+
+    return this.input.slice(position, this.position);
+  }
+
+  private readNumber(): string {
+    const position = this.position;
+    while (this.isDigit(this.ch)) {
+      this.readChar();
+    }
+
+    return this.input.slice(position, this.position);
+  }
+
+  private isLetter(ch: string): boolean {
+    return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'z') || ch === '_';
+  }
+
+  private isDigit(ch: string): boolean {
+    return '0' <= ch && ch <= '9';
+  }
+
+  private lookupIdent(ident: string): TokenItem {
+    if (Keywords[ident]) {
+      return Keywords[ident];
+    }
+
+    return TokenType.Ident;
+  }
+
+  private skipWhitespace() {
+    while (this.ch === ' ' || this.ch === '\t' || this.ch === '\n' || this.ch === '\r') {
+      this.readChar();
+    }
+  }
+}
